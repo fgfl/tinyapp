5928d225259c2be1438bcc9a71cc8609cab40efe
Frederick <frederick.gfl@gmail.com>
2019-12-09 23:13:08 +0000

    Add template file
    - created template HTML file to pass variables in using ejs

--- express_server.js
+++ express_server.js
@@ -5,38 +5,34 @@
  * https://web.compass.lighthouselabs.ca/days/w03d1/activities/169
  */
 
- const express = require('express');
- const app = express();
- const PORT = 8080; // default port 8080
+const express = require('express');
+const app = express();
+const PORT = 8080; // default port 8080
 
- app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
- const urlDatabase = {
-   'b2xVn2': 'http://www.lighthouselabs.ca',
-   '9sm5xK': 'http://www.google.com',
- };
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
+};
 
- app.get('/', (req, res) => {
-   res.send('Hello!');
- });
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
 
- app.get('/urls.json', (req, res) => {
-   res.json(urlDatabase);
- });
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
 
- app.get('/hello', (req, res) => {
-   res.send('<html><body>Hello <b>World</b></body></html>\n');
- });
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
 
- app.get('/set', (req, res) => {
-   const a = 1;
-   res.send(`a = ${a}`);
- });
+app.get('/urls', (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
+});
 
- app.get('/fetch', (req, res) => {
-   res.send(`a = ${a}`);
- });
-
- app.listen(PORT, () => {
-   console.log(`Example app listening to port ${PORT}`);
- });
+app.listen(PORT, () => {
+  console.log(`Example app listening to port ${PORT}`);
+});
