be45ab67dee6d53ee8e6059012296e192436baf2
Frederick <frederick.gfl@gmail.com>
2019-12-09 22:48:46 +0000

    Add example about accessing variable from oter cb funcs.

--- express_server.js
+++ express_server.js
@@ -26,6 +26,15 @@
    res.send('<html><body>Hello <b>World</b></body></html>\n');
  });
 
+ app.get('/set', (req, res) => {
+   const a = 1;
+   res.send(`a = ${a}`);
+ });
+
+ app.get('/fetch', (req, res) => {
+   res.send(`a = ${a}`);
+ });
+
  app.listen(PORT, () => {
    console.log(`Example app listening to port ${PORT}`);
  });
